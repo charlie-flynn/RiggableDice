748bbc3f7fddad242564b72b28167ecfe399f617
charlie-flynn <charlie.flynn.business@gmail.com>
2024-11-25 13:22:33 -0600

    die have been prepared and tested

--- RiggableDice/src/game/Dice.cs
+++ RiggableDice/src/game/Dice.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Raylib_cs;
+using MathLibrary;
 
 namespace RiggableDice
 {
     internal class Dice : Actor
     {
         private bool _isRolling = false;
+        private float _fontSize;
         private double _timer;
         private int _displayedNumber;
         private int _maxRoll;
         private int _rollCount;
         private int _result;
+        private int _riggedResult;
+        public bool IsRolling { get => _isRolling; set => _isRolling = value; }
 
         public Dice(int maxRoll)
         {
@@ -23,18 +29,65 @@ namespace RiggableDice
         public override void Start()
         {
             base.Start();
+            Transform.LocalScale = new Vector2(100, 100);
+            _fontSize = Transform.LocalScale.x / 4;
         }
 
         public override void Update(double deltaTime)
         {
             base.Update(deltaTime);
-
             // if it is rolling, change the displayed number every half a second
             // and increment the roll count
+            if (_isRolling)
+            {
+                if (_timer <= 0.0)
+                {
+                    if (_rollCount == 0)
+                        _result = DetermineRoll();
+
+                    _displayedNumber = RandomNumberGenerator.GetInt32(1, _maxRoll + 1);
+                    _rollCount++;
+                    _timer = 0.1;
+                }
+
+                _timer -= deltaTime;
+            }
 
             // when the roll count is equal to five or something, stop rolling and land on a number
             // if the dice was rigged, make it the specified value
+            // if it was rigged advantage style, make it no higher than the specified value but make sure one dice has the specified value
+            if (_rollCount == 10)
+            {
+                _isRolling = false;
+                _rollCount = 0;
+                _displayedNumber = _result;
+            }
 
+            // drawing
+
+            Vector2 offset = Transform.LocalScale / 2;
+            offset.x -= (_fontSize / 3.5f);
+            offset.y -= (_fontSize / 3.5f);
+
+            Raylib.DrawRectangleV(Transform.LocalPosition, Transform.LocalScale, Color.Green);
+            Raylib.DrawTextPro(new Font(), _displayedNumber.ToString(), offset, Transform.LocalPosition * -1, 0, _fontSize, 5, Color.White);
+
+        }
+
+        private int DetermineRoll()
+        {
+            int roll;
+
+            if (_riggedResult > 0 && _riggedResult <= _maxRoll)
+            {
+                roll = _riggedResult;
+            }
+            else
+            {
+                roll = RandomNumberGenerator.GetInt32(1, _maxRoll + 1);
+            }
+
+            return roll;
         }
     }
 }
--- RiggableDice/src/game/TitleScene.cs
+++ RiggableDice/src/game/TitleScene.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathLibrary;
+using Raylib_cs;
 
 namespace RiggableDice
 {
     internal class TitleScene : Scene
     {
+        Dice _dice;
         public override void Start()
         {
+
             base.Start();
+            _dice = (Dice)Actor.Instantiate(new Dice(20), null, new Vector2(50, 50));
         }
 
         public override void Update(double deltaTime)
         {
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+                _dice.IsRolling = true;
+
             base.Update(deltaTime);
         }
     }
